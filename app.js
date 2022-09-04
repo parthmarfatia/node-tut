04ce670b80da2c52ca7a8d4b3014622e5a782b85
Parth Marfatia <parthmarfatia153@gmail.com>
2022-09-04 16:21:18 +0530

    just

--- app.js
+++
@@ -1,7 +0,0 @@
-const data = [
-  { name: "parth", email: "go.com" },
-  { name: "parth", email: "go.com" },
-  { name: "parth", email: "go.com" },
-];
-
-module.exports = data;
--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const data = require("./app.js");
-http
-  .createServer((req, resp) => {
-    resp.writeHead(200, { "Content-Type": "application/json" });
-    resp.write(JSON.stringify(data));
-    resp.end();
-  })
-  .listen(5000);
